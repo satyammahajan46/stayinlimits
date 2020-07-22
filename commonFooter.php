b635c94d0f26f274fc9e52df49de70adf7bbcdee
satyammahajan46 <satyammahajan46@gmail.com>
2020-07-22 16:17:11 -0700

    fixes

--- commonFooter.php
+++ commonFooter.php
@@ -1,6 +1,6 @@
 <footer class = "footer">
     <ul class="list">
-    <li class="item">Designed and created By Satyam Mahajan</li>
+        <li class="item">Designed and created By Satyam Mahajan</li>
         <li class="item"><a class='item-link' href="http://facebook.com">&nbsp;Facebook</a></li>
         <li class="item"><a class='item-link' href="http://twitter.com">&nbsp;Twitter</a></li>
         <li class="item"><a class='item-link' href="http://youtube.com">&nbsp;Youtube</a></li>
